bc10f384f9db5df176758de56022ebcb6dd234cc
Beren Şenbay <150434390+berenSenbay@users.noreply.github.com>
2026-02-21 20:09:54 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -19,32 +19,15 @@ app.use('/api', router());
 
 const server = http.createServer(app);
 
-server.listen(8080, () => {
-    console.log('Server is running on http://localhost:8080');
-})
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://admin:adminpassword@cluster0.owymary.mongodb.net/?appName=Cluster0";
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    }
+
+mongoose.connect(uri).then(() => {
+    console.log("Successfully connected to MongoDB");
+    server.listen(8080, () => {
+        console.log('Server is running on http://localhost:8080');
+    });
+}).catch((err) => {
+    console.error("Failed to connect to MongoDB:", err);
 });
-async function run() {
-    try {
-        // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
-        // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-        // Ensures that the client will close when you finish/error
-        await client.close();
-    }
-}
-run().catch(console.dir);
 
 
