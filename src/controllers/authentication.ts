9fb2d99bcae86dc2584bc61e1fe1ad554871c0c1
Beren Şenbay <150434390+berenSenbay@users.noreply.github.com>
2026-02-23 15:09:31 +0300

    login

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -31,3 +31,30 @@ export const register = async (req: express.Request, res: express.Response) => {
         return res.sendStatus(500);
     }
 };
+
+export const login = async (req: express.Request, res: express.Response) => {
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return res.status(400).json({ message: 'Email and password are required' });
+        }
+        const user = await getUserByEmail(email).select('+authentication.salt +authentication.password');;
+        if (!user) {
+            return res.status(400).json({ message: 'Invalid email or password' });
+        }
+        const expectedHash = authentication(user.authentication.salt, password);
+        if (user.authentication.password !== expectedHash) {
+            return res.status(400).json({ message: 'Invalid email or password' });
+        }
+
+        const salt = random();
+        user.authentication.sessionToken = authentication(salt, user._id.toString());
+        await user.save();
+        res.cookie('sessionToken', user.authentication.sessionToken, { domain: process.env.DOMAIN, httpOnly: true, secure: true });
+        return res.status(200).json(user).end();
+
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(400);
+    }
+};
